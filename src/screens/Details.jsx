e6d45c03e80f482bd3fc59bf2f37e19702d9ab45
Mike Polinowski <mpolinowski@users.noreply.github.com>
2022-05-28 17:06:17 +0800

    Update Details.jsx

--- src/screens/Details.jsx
+++ src/screens/Details.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
-import { ListHeader, ListSubHeader, ListItem, ListButton } from '../components/_styles'
+import { ListHeader, ListSubHeader, ListItem } from '../components/_styles'
 
 const ItemDetails = (props) => {
     const item = props.navigation.getParam('data')
     console.log(item)
     return (
         <ListItem>
-            <ListButton onPress={() => {
-                props.navigation.navigate('Details', {data: item})
-            }} >
                 <ListHeader>{ item.fruit }</ListHeader>
                 <ListSubHeader>{ item.colour }</ListSubHeader>
-            </ListButton>
         </ListItem>
     )
 }
